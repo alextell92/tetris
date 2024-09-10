6fad974a5b48ceab9657ba9781a5d084242660eb
alex <83183043+alextell92@users.noreply.github.com>
2024-09-10 16:03:06 -0600

    .

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -6,35 +6,6 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-  piecePosition: { x: number, y: number }[];
-  intervalId: any; // Identificador del intervalo de temporizador
-
-  rows = 30;
-  columns = 10;
-  activePiece: number[][] = [
-    [1,1],
-    [1,1]
-  ];
-
-
-
-
-
-  constructor() {
-
- 
-    this.piecePosition = [
-      {y:0,x: 4 },
-      {y:0,x:5},  
-      {y:0,x:6},
-      {y:1,x:4}];// La pieza aparece en la fila superior, columna central
-
-     
-     
- 
-  }
-
-
   boardWidth = 10;  // 10 columnas
   boardHeight = 20; // 20 filas
   board: number[][] = [];
@@ -49,8 +20,24 @@ export class InicioComponent {
   ngOnInit(): void {
     this.createBoard();
     this.spawnPiece();
-    //this.startGame();
+    this.startGame();
   }
+
+  isCurrentPiece(row: number, col: number): boolean {
+    for (let y = 0; y < this.currentPiece.shape.length; y++) {
+      for (let x = 0; x < this.currentPiece.shape[y].length; x++) {
+        if (
+          this.currentPiece.shape[y][x] !== 0 &&
+          this.currentPiece.y + y === row &&
+          this.currentPiece.x + x === col
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
 
   createBoard() {
     // Inicializar el tablero vacío
@@ -123,77 +110,5 @@ export class InicioComponent {
         break;
     }
   }
-
-
-  isCurrentPiece(row: number, col: number): boolean {
-    for (let y = 0; y < this.currentPiece.shape.length; y++) {
-      for (let x = 0; x < this.currentPiece.shape[y].length; x++) {
-        if (
-          this.currentPiece.shape[y][x] !== 0 &&
-          this.currentPiece.y + y === row &&
-          this.currentPiece.x + x === col
-        ) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-  
-
-
-  piezas(){
-    const L = [
-      { y:0,x: 4 },
-      {y:0,x:5},  
-      {y:0,x:6},
-      {y:1,x:4}]; // La pieza aparece en la fila superior, columna central
-
-     const cuadro = [
-        { y:0,x: 4 },
-        {y:0,x:5},  
-        {y:1,x:5},
-        {y:1,x:4}]; // La pieza aparece en la fila superior, columna central
-  
-        
-    const l = [
-      { y:0,x: 4 },  
-      {y:1,x:4},
-      {y:2,x:4}]; // La pieza aparece en la fila superior, columna central
-
-      const Z = [
-        { y:0,x: 4 },
-        { y:0,x: 5},
-        {y:1,x:4},
-        {y:1,x:3}]; // La pieza aparece en la fila superior, columna central
-  
-
-  }
-
-
-
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-
-
-
-
-
-  // Verifica si la posición es válida
-  isValidPosition(x: number, y: number): boolean {
-
-    // console.log("X:"+x)
-    // console.log("Y:"+y)
-    // console.log(x >= 0 && x < 15 && y >= 0 && y< 20)
-    return x >= 0 && x < 15 && y >= 0 && y< 15;
-  }
-
-
-
-
-
 
 }
